dfd3c677e6694ba9ebfd91411526800fe5ac47b5
Uralberto <urievalo@hotmail.com>
2020-11-01 20:35:23 -0500

    Organizando gitignore, mejorando hermelinda.js

--- docs/hermelinda2.js
+++ docs/hermelinda2.js
@@ -1,16 +1,21 @@
-
-      const piezasUno = document.querySelectorAll("path");
+      
+      
+      const momDosPaths = document.getElementsByClassName("mom-dos-path");
       const btn = document.getElementById("momContainer_boton")
 
-      console.log(piezasUno)
+      console.log(momDosPaths)
 
       function ocultarPaths() {
-        for (let j = 0; j <= piezasUno.length - 1; j++) {
-          piezasUno[j].style.opacity = 0.03;
+        for (let i = 0; i <= momDosPaths.length - 1; i++) {
+          momDosPaths[i].style.opacity = 0.03;
         }
       }
 
-
+      function mostrarPathsOriginales() {
+        for (let i = 0; i <= momDosPaths.length - 1; i++) {
+          momDosPaths[i].style.opacity = 1;
+        }
+      }
 
       function colorRandom() {
       let set = "0123456789ABCDEF";
@@ -28,25 +33,25 @@
         ocultarPaths();
         let i = 0;
         let setInt = setInterval(function () {
-          piezasUno[i].style.opacity = 1;
+          momDosPaths[i].style.opacity = 1;
           let color = colorRandom()
-          piezasUno[147].style.fill = color;
-          piezasUno[148].style.fill = color;
-          piezasUno[146].style.fill = color;
-          piezasUno[145].style.fill = color;
-          piezasUno[228].style.fill = color;
-          piezasUno[225].style.fill = color;
-          piezasUno[224].style.fill = color;
-          piezasUno[223].style.fill = color;
-          if (i === piezasUno.length - 1) {
+          momDosPaths[147].style.fill = color;
+          momDosPaths[148].style.fill = color;
+          momDosPaths[146].style.fill = color;
+          momDosPaths[145].style.fill = color;
+          momDosPaths[228].style.fill = color;
+          momDosPaths[225].style.fill = color;
+          momDosPaths[227].style.fill = color;
+          momDosPaths[226].style.fill = color;
+          momDosPaths[229].style.fill = color;
+
+          if (i === momDosPaths.length - 1) {
             clearInterval(setInt);
           }
           i++;
         }, 10);
       }
       
-
-
 let tiempoCiclo;
 
       function pintarMamaCiclo() {
@@ -71,6 +76,8 @@ let tiempoCiclo;
           btn.innerHTML = '<i class="far fa-pause-circle"></i>';
         } else {
           detenerPintarMamaCiclos();
+          mostrarPathsOriginales()
+
           check = 0;
 
           btn.innerHTML = '<i class="far fa-play-circle"></i>';
